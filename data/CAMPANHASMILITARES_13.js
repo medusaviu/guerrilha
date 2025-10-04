d4b59fd9d68a026b5ffa269af9c5b5469e72b4f8
medusaviu <jeniferslvf@gmail.com>
2025-10-04 12:53:09 -0300

    guerrilha

--- data/CAMPANHASMILITARES_13.js
+++
@@ -1,9 +0,0 @@
-var json_CAMPANHASMILITARES_13 = {
-"type": "FeatureCollection",
-"name": "CAMPANHASMILITARES_13",
-"features": [
-{ "type": "Feature", "properties": { "id": null, "ANO": "1973", "LOCAL": "Serra das Andorinhas", "ACONTECIME": "Segunda Campanha, com Maior Repressão e Uso de Tortura." }, "geometry": { "type": "Point", "coordinates": [ -48.586733466980988, -6.215580085418178 ] } },
-{ "type": "Feature", "properties": { "id": null, "ANO": "1974", "LOCAL": "Toda a Região do Araguaia", "ACONTECIME": "Terceira Campanha, Eliminação Final dos Guerrilheiros." }, "geometry": { "type": "Point", "coordinates": [ -48.311652447401755, -5.864254542509631 ] } },
-{ "type": "Feature", "properties": { "id": null, "ANO": "1972", "LOCAL": null, "ACONTECIME": "Primeira Ofensiva Militar (Mal Preparada, levando ao Fracasso)" }, "geometry": { "type": "Point", "coordinates": [ -48.790424745064641, -5.485651412464029 ] } }
-]
-}
