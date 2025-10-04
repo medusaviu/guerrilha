f5561b746a13c90f80c23033c973ebf98de39249
medusaviu <jeniferslvf@gmail.com>
2025-10-04 10:41:25 -0300

    Mapa interativo Guerrilha do Araguaia - versão limpa

---
+++ data/MEMRIAEJUSTIA_12.js
@@ -0,0 +1,10 @@
+var json_MEMRIAEJUSTIA_12 = {
+"type": "FeatureCollection",
+"name": "MEMRIAEJUSTIA_12",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "LOCAL": "MEMORIAL", "INFORMAÇ�": "Monumentos e Placas em Homenagem aos Guerrilheiros.", "FOTO": "C:\\Users\\jenif\\OneDrive\\Área de Trabalho\\UNIVERIDADE\\PROJETOS CARTOGRÁFICOS - TRABALHO\\MAPA JORGE PINHEIRO\\camada 5\\memorial.png" }, "geometry": { "type": "Point", "coordinates": [ -48.566895221790475, -6.396534219806211 ] } },
+{ "type": "Feature", "properties": { "id": null, "LOCAL": "COMISSÃO DA VERDADE LOCAL", "INFORMAÇ�": "Investigações Sobre os Desaparecidos.", "FOTO": "C:\\Users\\jenif\\OneDrive\\Área de Trabalho\\UNIVERIDADE\\PROJETOS CARTOGRÁFICOS - TRABALHO\\MAPA JORGE PINHEIRO\\camada 3\\DSC_0292_reduzida-1024x683.jpg" }, "geometry": { "type": "Point", "coordinates": [ -49.762778171307225, -5.609710297881186 ] } },
+{ "type": "Feature", "properties": { "id": null, "LOCAL": "COMISSÃO DA VERDADE NACIONAL", "INFORMAÇ�": "Documentos Sobre a Repressão e Desaparecidos.", "FOTO": "C:\\Users\\jenif\\OneDrive\\Área de Trabalho\\UNIVERIDADE\\PROJETOS CARTOGRÁFICOS - TRABALHO\\MAPA JORGE PINHEIRO\\camada 3\\membros-comissao-nacional-verdade.jpeg" }, "geometry": { "type": "Point", "coordinates": [ -47.87395369964063, -15.76130359737696 ] } }
+]
+}
