c7c1a2f2abb75c7367035552ae0ed78f52de4b47
medusaviu <jeniferslvf@gmail.com>
2025-10-04 12:58:57 -0300

    guerrilha

---
+++ qgis2web_2025_10_04-12_57_56_141257/data/BASEMILITAR_17.js
@@ -0,0 +1,8 @@
+var json_BASEMILITAR_17 = {
+"type": "FeatureCollection",
+"name": "BASEMILITAR_17",
+"features": [
+{ "type": "Feature", "properties": { "id": null, "NOME": "BACABA" }, "geometry": { "type": "Point", "coordinates": [ -48.470908751569588, -6.14702601628187 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOME": "XAMBIOÁ" }, "geometry": { "type": "Point", "coordinates": [ -48.526093440540222, -6.411180214672245 ] } }
+]
+}
---
+++ qgis2web_2025_10_04-12_57_56_141257/data/BASESGUERRILHEIRAS_16.js
@@ -0,0 +1,14 @@
+var json_BASESGUERRILHEIRAS_16 = {
+"type": "FeatureCollection",
+"name": "BASESGUERRILHEIRAS_16",
+"features": [
+{ "type": "Feature", "properties": { "id": null, "DESTACAMEN": "DESTACAMENTO A", "A": "CHEGA COM JEITO", "B": null, "C": null }, "geometry": { "type": "Point", "coordinates": [ -48.484887827939446, -6.244848482371949 ] } },
+{ "type": "Feature", "properties": { "id": null, "DESTACAMEN": "DESTACAMENTO A", "A": "METADE", "B": null, "C": null }, "geometry": { "type": "Point", "coordinates": [ -48.511711370732343, -6.21690729196268 ] } },
+{ "type": "Feature", "properties": { "id": null, "DESTACAMEN": "DESTACAMENTO A", "A": "FAVEIRA", "B": null, "C": null }, "geometry": { "type": "Point", "coordinates": [ -48.490785701151431, -6.093672098983214 ] } },
+{ "type": "Feature", "properties": { "id": null, "DESTACAMEN": "DESTACAMENTO B", "A": null, "B": "GAMELEIRA", "C": null }, "geometry": { "type": "Point", "coordinates": [ -48.523446670704232, -6.363039717803158 ] } },
+{ "type": "Feature", "properties": { "id": null, "DESTACAMEN": "DESTACAMENTO B", "A": null, "B": "COURO D'ANTAS", "C": null }, "geometry": { "type": "Point", "coordinates": [ -48.520862664334516, -6.351810904736984 ] } },
+{ "type": "Feature", "properties": { "id": null, "DESTACAMEN": "DESTACAMENTO C", "A": null, "B": null, "C": "PAU PRETO" }, "geometry": { "type": "Point", "coordinates": [ -48.592461411015123, -6.408025034362081 ] } },
+{ "type": "Feature", "properties": { "id": null, "DESTACAMEN": "DESTACAMENTO C", "A": null, "B": null, "C": "ABÓBORA" }, "geometry": { "type": "Point", "coordinates": [ -48.598539690989902, -6.257395394223192 ] } },
+{ "type": "Feature", "properties": { "id": null, "DESTACAMEN": "DESTACAMENTO C", "A": null, "B": null, "C": "ESPERANCINHA" }, "geometry": { "type": "Point", "coordinates": [ -48.583893517613802, -6.242749220847092 ] } }
+]
+}
